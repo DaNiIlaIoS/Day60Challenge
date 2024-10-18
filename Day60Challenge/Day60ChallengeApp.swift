69b335a1c3ca5de45b402b42299f74f7af750035
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-10-18 18:57:57 +0500

    Initial Commit

---
+++ Day60Challenge/Day60ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60ChallengeApp.swift
+//  Day60Challenge
+//
+//  Created by Даниил Сивожелезов on 18.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Day60ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
