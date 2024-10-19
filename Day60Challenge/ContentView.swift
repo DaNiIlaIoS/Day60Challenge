88f022e6202df926310b5581f29ea0126dc9fa41
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-10-19 13:50:55 +0500

    Fetch the data and parse it into User and Friend structs.

--- Day60Challenge/ContentView.swift
+++ Day60Challenge/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                ForEach(users, id: \.id) { user in
+                    Text(user.name)
+                }
+            }
+        .task {
+            await getUsers()
         }
-        .padding()
+    }
+    
+    func getUsers() async {
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decoder = try? JSONDecoder().decode([User].self, from: data) {
+                users = decoder
+            }
+            
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
